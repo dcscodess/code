b0c5d107f951af0a1dc51c5b819a425220529a87
dcscodess <cleverlytics@gmail.com>
2025-05-11 23:42:25 +0530

    Update Footer.tsx
    dev

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className='tw-sticky tw-bottom-0 tw-py-1'>
       <div className='tw-container-lg tw-flex tw-justify-evenly tw-max-auto'>
-        <Box sx={{ paddingBlock: '5px' }}>Copyright &copy; 2025 Leetcode</Box>
-        <Box sx={{ paddingBlock: '5px' }}>Made with Love ❤</Box>
+        <Box sx={{ paddingBlock: '5px' }}>Copyright &copy; 2025</Box>
+        <Box sx={{ paddingBlock: '5px' }}>Playground for programming by DLithe</Box>
       </div>
     </footer>
   );
